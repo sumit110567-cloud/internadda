8a66652fe546341af2e6921d6857adad831f842c
sumit110567-cloud <sumit110567@gmail.com>
2026-02-15 18:18:02 +0530

    Update TrustBadges.tsx

--- components/blog/TrustBadges.tsx
+++ components/blog/TrustBadges.tsx
@@ -6,7 +6,7 @@ export function TrustBadges() {
       <h4 className="font-bold text-gray-900 mb-4">Trusted by 72,000+ students</h4>
       <div className="flex justify-center gap-6 flex-wrap">
         <Image src="/company3.jpg" alt="MSME" width={60} height={30} className="opacity-80 hover:opacity-100 transition" />
-        <Image src="https://upload.wikimedia.org/wikipedia/commons/7/7f/Tracxn_Logo.png?20150621095748" alt="Teacxn" width={60} height={30} className="opacity-80 hover:opacity-100 transition" />
+        <Image src="/Tracxn_Logo.png" alt="Teacxn" width={60} height={30} className="opacity-80 hover:opacity-100 transition" />
         <Image src="/company2.jpg" alt="LAREX" width={60} height={30} className="opacity-80 hover:opacity-100 transition" />
 
       </div>
