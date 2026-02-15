ead211be9ffd21e6273910a87c83a8c3452d6523
sumit110567-cloud <sumit110567@gmail.com>
2026-02-15 18:22:54 +0530

    Update TrustBadges.tsx

--- components/blog/TrustBadges.tsx
+++ components/blog/TrustBadges.tsx
@@ -6,7 +6,7 @@ export function TrustBadges() {
       <h4 className="font-bold text-gray-900 mb-4">Trusted by 7,200+ students</h4>
       <div className="flex justify-center gap-6 flex-wrap">
         <Image src="/company3.jpg" alt="MSME" width={60} height={30} className="opacity-80 hover:opacity-100 transition" />
-        <Image src="/Tracxn_Logo.png" alt="Teacxn" width={120} height={25} className="opacity-80 hover:opacity-100 transition" />
+        <Image src="/Tracxn_Logo.png" alt="Teacxn" width={110} height={15} className="opacity-80 hover:opacity-100 transition" />
         <Image src="/company2.jpg" alt="LAREX" width={60} height={30} className="opacity-80 hover:opacity-100 transition" />
 
       </div>
