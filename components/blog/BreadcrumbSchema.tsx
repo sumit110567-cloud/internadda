c2ff1245656ce4a28e7b44e6ec0b3a526d63529a
sumit110567-cloud <sumit110567@gmail.com>
2026-02-15 17:08:55 +0530

    Create BreadcrumbSchema.tsx

---
+++ components/blog/BreadcrumbSchema.tsx
@@ -0,0 +1,18 @@
+interface BreadcrumbItem {
+  name: string;
+  url: string;
+}
+
+export function BreadcrumbSchema({ items }: { items: BreadcrumbItem[] }) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": items.map((item, index) => ({
+      "@type": "ListItem",
+      "position": index + 1,
+      "name": item.name,
+      "item": `https://internadda.com${item.url}`,
+    })),
+  };
+  return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />;
+}
